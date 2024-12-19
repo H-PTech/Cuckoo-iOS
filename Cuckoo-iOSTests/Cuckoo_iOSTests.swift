171af59635aaa01f13cf14442cb4012a413764ed
gomminjae <gommj0611@naver.com>
2024-12-20 01:05:30 +0900

    Initial Commit

---
+++ Cuckoo-iOSTests/Cuckoo_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cuckoo_iOSTests.swift
+//  Cuckoo-iOSTests
+//
+//  Created by 권민재 on 12/20/24.
+//
+
+import Testing
+@testable import Cuckoo_iOS
+
+struct Cuckoo_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
