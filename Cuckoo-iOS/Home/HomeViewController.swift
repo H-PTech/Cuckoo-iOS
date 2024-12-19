3f3637575376c2742cc585530bd7e763aa6e8661
gomminjae <gommj0611@naver.com>
2024-12-20 02:57:58 +0900

    Layout Setting

---
+++ Cuckoo-iOS/Home/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  Cuckoo-iOS
+//
+//  Created by 권민재 on 12/20/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import FlexLayout
+import PinLayout
+
+
+class HomeViewController: UIViewController {
+    
+    private let containerView = UIView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(containerView)
+
+    }
+    
+
+}
---
+++ Pods/Target Support Files/Pods-Cuckoo-iOS-Cuckoo-iOSUITests/Pods-Cuckoo-iOS-Cuckoo-iOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cuckoo_iOS_Cuckoo_iOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Cuckoo_iOS_Cuckoo_iOSUITests
+@end
